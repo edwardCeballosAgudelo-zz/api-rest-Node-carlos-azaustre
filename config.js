ded23a21d220ede49aacb04a8aa1313c11fbc616
edward ceballos <edwardceballos@edwards-MacBook-Pro.local>
2017-08-23 20:33:37 -0500

    change npm script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
+    db: process.env.MONGODB_URI || 'mongodb://edwardceb:!QAZxsw2@ds023463.mlab.com:23463/shop',
     SECRET_TOKEN: 'miclavedetokens'
 }
